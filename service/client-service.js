83ce85115e36b8733110f3dd716e7c7d4ea82cc1
alexabadp <aalex.abad96@gmail.com>
2023-04-04 00:28:54 -0500

    asincronismo

--- service/client-service.js
+++ service/client-service.js
@@ -21,10 +21,30 @@ const eliminarCliente = (id) => {
   });
 };
 
+const detalleCliente = (id) => {
+  return fetch(`http://localhost:3000/perfil/${id}`).then((respuesta) =>
+    respuesta.json()
+  );
+};
+
+const actualizarCliente = (nombre, email, id) => {
+  return fetch(`http://localhost:3000/perfil/${id}`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ nombre, email }),
+  })
+    .then((respuesta) => respuesta)
+    .catch((err) => console.log(err));
+};
+
 export const clientServices = {
   listClientes,
   crearCliente,
   eliminarCliente,
+  detalleCliente,
+  actualizarCliente,
 };
 
 /////////////////////////////////////////////
